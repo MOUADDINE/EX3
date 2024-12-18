07e963c02e67f5ddda8827864345fa6a5e14c32a
soufiane <soufianemouaddinepro@gmail.com>
2024-12-18 12:15:32 +0100

    etap8

--- src/main/java/nbroman/ArabicRomanNumerals.java
+++ src/main/java/nbroman/ArabicRomanNumerals.java
@@ -4,6 +4,7 @@ import javax.swing.text.DefaultStyledDocument;
 
 public class ArabicRomanNumerals {
     public String convert(int nbr){
+        // Tests de conversions simples
         if(nbr==1) return "I";
         else if(nbr==2) return "II";
         else if(nbr==4) return "IV";
@@ -11,6 +12,7 @@ public class ArabicRomanNumerals {
         else if (nbr==9) return "IX";
         else if (nbr==10) return "X";
         else if (nbr==40) return "XL";
+        else if (nbr==50) return "L";
         else return null;
     }
 
--- src/test/java/nbroman/ArabicRomanNumeralsTest.java
+++ src/test/java/nbroman/ArabicRomanNumeralsTest.java
@@ -8,9 +8,9 @@ public class ArabicRomanNumeralsTest {
         //GIVEN
         ArabicRomanNumerals converter = new ArabicRomanNumerals();
         //WHEN
-        String rst =converter.convert(40);
+        String rst =converter.convert(50);
         //THEN
-        assertThat(rst).isEqualTo("XL");
+        assertThat(rst).isEqualTo("L");
     }
 
 
