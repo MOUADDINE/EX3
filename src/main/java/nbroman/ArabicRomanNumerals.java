6841004f85eff005a57e28491b279e6e540d4ba8
soufiane <soufianemouaddinepro@gmail.com>
2024-12-18 11:49:19 +0100

    etap3

--- src/main/java/nbroman/ArabicRomanNumerals.java
+++ src/main/java/nbroman/ArabicRomanNumerals.java
@@ -4,6 +4,7 @@ public class ArabicRomanNumerals {
     public String convert(int nbr){
         if(nbr==1) return "I";
         else if(nbr==2) return "II";
+        else if(nbr==4) return "IV";
         else return null;
     }
 
--- src/test/java/nbroman/ArabicRomanNumeralsTest.java
+++ src/test/java/nbroman/ArabicRomanNumeralsTest.java
@@ -8,9 +8,9 @@ public class ArabicRomanNumeralsTest {
         //GIVEN
         ArabicRomanNumerals converter = new ArabicRomanNumerals();
         //WHEN
-        String rst =converter.convert(2);
+        String rst =converter.convert(4);
         //THEN
-        assertThat(rst).isEqualTo("II");
+        assertThat(rst).isEqualTo("IV");
     }
 
 
